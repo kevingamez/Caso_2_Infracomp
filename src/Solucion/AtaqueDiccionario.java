ec3dd9f6684feb2469508e4b58b07dc9808c84fa
sharkstorm1010 <59325966+sharkstorm1010@users.noreply.github.com>
2020-10-29 22:05:32 -0500

    Se añade la librería de Apache Derby y algunos métodos para ataque por diccionario.

--- src/Solucion/AtaqueDiccionario.java
+++ src/Solucion/AtaqueDiccionario.java
@@ -1,37 +1,207 @@
 package Solucion;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.sql.*;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.management.Query;
+
+/**
+ * Clase que maneja la base de datos con la que se realizarán ataques de diccionario para disminuir el tiempo de espera de palabras comunes.
+ * @author Sergio Julian Zona Moreno y Kevin Steven Gamez Abril
+ */
 public class AtaqueDiccionario {
 
-	public Connection CrearDB() {
-		Connection con;
-		File url = new File("./data/baseDatos");
-		if (url.exists()) {
+	
+	public final static String SQL = "javax.jdo.query.SQL";
+	/**
+	 * Set de palabras para evitar repetición
+	 */
+	static HashSet<String>set;
+	
+	/**
+	 * Conexión con la base de datos
+	 */
+	static Connection con;
+	
+	/**
+	 * Método constructor que conecta la base de datos.
+	 * @throws Exception
+	 */
+	public AtaqueDiccionario() throws Exception
+	{
+		set = new HashSet<String>();
+		conectarBaseDeDatos();
+	}
+	
+	/**
+	 * Método que crea la base de datos.
+	 * @return Base de datos creada.
+	 */
+	public static Connection CrearDB() {
+		
+		String path ="./data/baseDatos/Datos";
+		File url = new File(path);
+		if (url.exists()) 
+		{
 			System.out.println("Base de datos ya existe");
-		} else {
-
-			try {
+		} 
+		else 
+		{
+			try 
+			{
 				Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-				String db = "jdbc:derby:./data/baseDatos;create=true";
+				String db = "jdbc:derby:"+path+";create=true";
 				con = DriverManager.getConnection(db);
-
-				String tabla = "create table HashCode( id INT PRIMARY KEY, Palabra VARCHAR2(7), MD5 VARCHAR(32), SHA_256 VARCHAR2(64), SHA_384 VARCHAR2(96), SHA_512 VARCHAR2(128) )";
-				PreparedStatement ps = con.prepareStatement(tabla);
-				ps.execute();
-				ps.close();
-
-				System.out.println("Base de datos creada");
+				Statement stmt = con.createStatement();
+				
+				
+				String queryCreacion = "CREATE TABLE HashCode"
+										+"(ID INT PRIMARY KEY,"
+										+"Palabra VARCHAR(7),"
+										+"MD5 VARCHAR(32),"
+										+"SHA_256 VARCHAR(64),"
+	                                    +"SHA_384 VARCHAR(96),"
+	                                    +"SHA_512 VARCHAR(128))";                     		
+				stmt.execute(queryCreacion);
+				System.out.println("Base de datos creada.");
+				System.out.println("La tabla fue creada con éxito.");
 				return con;
-
-			} catch (Exception e) {
+			} 
+			catch (Exception e) 
+			{
 				Logger.getLogger(AtaqueDiccionario.class.getName()).log(Level.SEVERE, null, e);
 			}
 		}
 		return null;
 	}
+	
+	/**
+	 * Método que conecta la base de datos inicializando la conexión con la instancia deseada.
+	 * @throws Exception Conexión que será añadida.
+	 */
+	public static void conectarBaseDeDatos() throws Exception
+	{
+		String path ="./data/baseDatos/Datos";
+		String db = "jdbc:derby:"+path+";";
+		con = DriverManager.getConnection(db);
+		System.out.println("Se ha conectado exitosamente a la base de datos.");
+	}
+	
+	/**
+	 * Método que inserta todas las tuplas de datos en la base de datos.
+	 */
+	public static void insertarValores() 
+	{
+		//Lectura de los diccionarios.
+		String pathEspanol = "./data/espanol.txt";
+		String pathIngles = "./data/ingles.txt";
+		String linea;
+		Statement stmt;
+		try 
+		{
+			FileReader f = new FileReader(pathEspanol);
+			BufferedReader b = new BufferedReader(f);
+			while((linea = b.readLine()) != null) {
+				if(linea.trim().length() < 8) {
+					set.add(linea);
+				}
+			}
+			f = new FileReader(pathIngles);
+			b = new BufferedReader(f);
+			while((linea = b.readLine()) != null) {
+				if(linea.trim().length() < 8) {
+					set.add(linea);
+				}
+			}
+			b.close();
+			f.close();
+			
+			//Inserción de los datos en la tabla.
+			Iterator<String> iterator=set.iterator();
+			stmt = con.createStatement();
+			int i=1;
+			while(iterator.hasNext())
+			{
+				++i;
+				String palabra=iterator.next();
+				System.out.println(palabra);
+				String ejecucion = "INSERT INTO HashCode (id, MD5, SHA-256, SHA-384, SHA-512) "
+						+ "values ("+i+",'"
+						+ Hash.imprimirHexa(Hash.generar_codigo(palabra, "MD5"))+"','"
+						+ Hash.imprimirHexa(Hash.generar_codigo(palabra, "SHA-256"))+"','"
+						+ Hash.imprimirHexa(Hash.generar_codigo(palabra, "SHA-384"))+"','"
+						+ Hash.imprimirHexa(Hash.generar_codigo(palabra, "SHA-512"))+"')";
+				stmt.execute(ejecucion);
+			}	
+			System.out.println("\nEl proceso ha terminado exitosamente.");
+		} 
+		catch (Exception e) {
 
+		}		
+	}
+	
+	/**
+	 * Método que retorna todas las tuplas que se encuentren con el algoritmo deseado.
+	 * @throws Exception
+	 */
+	public static void obtenerValor(String nombreAlgoritmo, String cadena) throws Exception 
+	{
+		Statement stmt = con.createStatement();
+		String query = "SELECT Palabra FROM HashCode WHERE "+nombreAlgoritmo+"="+cadena;
+	    ResultSet rs = stmt.executeQuery(query);
+	    while(rs.next()) {
+	         System.out.println(nombreAlgoritmo+": "+rs.getString(nombreAlgoritmo));
+	      }
+	}
+	
+	/**
+	 * Método que selecciona todas las entradas de la tabla de la base de datos.
+	 * @throws Exception si no se puede completar la acción.
+	 */
+	public static void datosTabla() 
+	{
+		try
+		{
+			Statement stmt = con.createStatement();
+			String query = "SELECT * FROM HashCode";
+			ResultSet rs = stmt.executeQuery(query);
+		    while(rs.next()) {
+		         System.out.println(rs.getString("Palabra"));
+		      }
+		    System.out.println("Se obtuvieron los datos de la tabla.");
+		   
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}		
+	}
+	
+	
+	/**
+	 * Método que borra las entradas de la tabla de la base de datos.
+	 * @throws Exception si no se puede completar la acción.
+	 */
+	public static void borrarTabla() 
+	{
+		 
+		try
+		{
+			Statement stmt = con.createStatement();
+			String query = "DELETE FROM HashCode";
+		    stmt.execute(query);
+		    System.out.println("Se eliminaron los datos de la tabla.");
+		   
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}		
+	}
 }
--- src/Solucion/Main.java
+++ src/Solucion/Main.java
@@ -11,16 +11,18 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
+	
 	/**
 	 * Método main de la aplicación.
 	 * 
 	 * @param arg
+	 * @throws Exception 
 	 */
-	public static void main(String[] arg) {
+	public static void main(String[] arg) throws Exception {
 		String mensaje;
 		String algoritmo = "";
 		byte[] codigoCriptograficoHash = null;
-
+		AtaqueDiccionario ataque= new AtaqueDiccionario();
 		try {
 			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 			String line = "";
@@ -35,12 +37,20 @@ public class Main {
 					// Generar código criptográfico del mensaje.
 					System.out.println("Ingrese el mensaje:");
 					mensaje = bf.readLine().trim();
-					if (!mensaje.matches("[a-z]*")) {
-						System.out.println("Por favor ingrese un valor entre ('a-z')");
+					//Excepciones de cadenas
+					if (mensaje.length() > 7 || !mensaje.matches("[a-z]*"))
+					{
+						if(mensaje.length() > 7 )
+						{
+							System.out.println("Por favor ingrese una cadena con menos de 7 caracteres.");
+						}
+						if(!mensaje.matches("[a-z]*")) 
+						{ 
+							System.out.println("Por favor ingrese un valor entre ('a-z')"); 
+						} 						
 					}
-					if (mensaje.length() > 7) {
-						System.out.println("Por favor ingrese una cadena con menos de 7 caracteres.");
-					} else {
+					else 
+					{
 						System.out.println("Ingrese el nombre del algoritmo (i.e: MD5, SHA-256, SHA-384, SHA-512):");
 						algoritmo = bf.readLine().toUpperCase().trim();
 
@@ -53,9 +63,12 @@ public class Main {
 								"Longitud del código generado: " + Hash.imprimirHexa(codigoCriptograficoHash).length());
 						System.out.println("El proceso de encriptado tardó: " + (fin - inicio) + " milisegundos\n");
 					}
-				} else if (Integer.parseInt(line) == 2) {
+				} 
+				else if (Integer.parseInt(line) == 2) 
+				{
 					// Obtener código criptográfico del mensaje.
-					if (codigoCriptograficoHash != null) {
+					if (codigoCriptograficoHash != null) 
+					{
 						long inicio = System.currentTimeMillis();
 						String desencriptado = Hash.identificar_entrada(codigoCriptograficoHash, algoritmo);
 						long fin = System.currentTimeMillis();
@@ -66,13 +79,28 @@ public class Main {
 					} else {
 						System.out.println("Por favor ejecute el paso 2 antes de realizar este proceso.");
 					}
-				} else if (Integer.parseInt(line) == 3) {
+				} 
+				else if (Integer.parseInt(line) == 3) 
+				{
 					leer = false;
 				}
+				else if (Integer.parseInt(line) == 4)
+				{
+					ataque.borrarTabla();
+				}
+				else if (Integer.parseInt(line) == 5)
+				{
+					ataque.insertarValores();
+				}
+				else if (Integer.parseInt(line) == 6)
+				{
+					ataque.datosTabla();
+				}
 			}
-		} catch (Exception e) {
-			System.out
-					.println("Ha ocurrido un error inesperado \nPor favor reinicie la aplicación e intente de nuevo\n");
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("Ha ocurrido un error inesperado \nPor favor reinicie la aplicación e intente de nuevo\n");
 		}
 	}
 }
